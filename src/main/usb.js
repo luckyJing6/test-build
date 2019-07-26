466686b4319f9796da32c3a2b10dd72e9a7e4cbc
郭靖 <1213407593@qq.com>
2019-07-26 10:37:23 +0800

    --

--- src/main/usb.js
+++ src/main/usb.js
@@ -1,5 +1,7 @@
-const usb = require('usb')
-var list = usb.getDeviceList()
-console.log('sub list', list)
+// const usb = require('usb')
+// var list = usb.getDeviceList()
+// console.log('sub list', list)
 const serail = require('serialport')
 console.log(serail)
+// - sudo apt-get install build-essential libudev-dev -y
+// 
