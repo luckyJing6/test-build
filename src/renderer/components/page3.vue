01091acf74bdc5dc252d309041eabc1ee5d1f45d
郭靖 <1213407593@qq.com>
2019-09-04 11:39:30 +0800

    0.2.8

--- src/renderer/components/page3.vue
+++ src/renderer/components/page3.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
+  <div class="page3">
     <div>我是页面page3</div>
     <div v-for="item in 5" :key="item">我是{{item}}</div>
-    <div class="image">
-      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557813260635&di=399d5201bedb1501231f213f6f877658&imgtype=0&src=http%3A%2F%2Fimg.atobo.com%2FProductImg%2FEWM%2FUWeb%2F3%2F8%2F4%2F0%2F5355%2F38405355%2F1.gif" width="100" height="100">
-    </div>
   </div>
 </template>
 
@@ -18,5 +15,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+}
+.page3 {
+  width: 100%;
+  height: 100vh;
+  background: blue;
+  overflow: hidden;
+}
 </style>
